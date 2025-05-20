06553904a6b77904e6a2944183250ac599625e24
Herculez <herculezarmy@gmail.com>
2025-05-20 15:22:28 +1000

    Added testing functionality with the Person class and addPerson Func using JUnit5

---
+++ src/test/java/PersonTest.java
@@ -0,0 +1,42 @@
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class PersonTest {
+    
+    //Valid ID, ADDRESS, BIRTHDATE
+    @Test
+    public void addPersonValidTest(){
+        Person person = new Person();
+        boolean result = person.addPerson("23%%%%%%EE", "1|Street|City|Victoria|australia", "29-2-2000");
+
+        assertTrue(result);
+    }
+
+    //Invalid ID, VALID ADDRESS, VALID BIRTHDATE
+    @Test
+    public void addPersonInvalidIDTest(){
+        Person person = new Person();
+        boolean result = person.addPerson("11%%%%%%EE", "1|Street|City|Victoria|australia", "29-2-2000");
+
+        assertFalse(result);
+    }
+
+    //Valid ID, INVAlID ADDRESS, BIRTHDATE
+    @Test
+    public void addPersonInvalidAddressTest(){
+        Person person = new Person();
+        boolean result = person.addPerson("23%%%%%%EE", "1|Street|City|New South Wales|australia", "29-2-2000");
+
+        assertFalse(result);
+    }
+
+    //Valid ID, ADDRESS, INVALID BIRTHDATE
+    @Test
+    public void addPersonInvalidBirthdateTest(){
+        Person person = new Person();
+        boolean result = person.addPerson("23%%%%%%EE", "1|Street|City|Victoria|australia", "0-2-2000");
+
+        assertFalse(result);
+    }
+}
