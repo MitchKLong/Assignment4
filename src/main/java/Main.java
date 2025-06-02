10e15c63f982e3c85613727cc5ddf7588e5624f9
Jessica <jessica040806@gmail.com>
2025-06-02 12:49:47 +1000

    Remove unused imports

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,4 @@
-import java.util.Date;
-import java.util.HashMap;
 import java.util.Scanner;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 
 public class Main{
 
